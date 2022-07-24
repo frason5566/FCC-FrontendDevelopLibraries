a1ddaedbd04a7740d201cb7b3ff51fe8546f9019
KKITC-Frason <50657080+KKITC-Frason@users.noreply.github.com>
2022-07-24 16:56:26 +0800

    0724

---
+++ my-app/src/Pages/Home/Components/List.js
@@ -0,0 +1,7 @@
+// import React from "react"
+// import Item from './Components/Item'
+
+const List = () =>{
+    return <div>List</div>
+} 
+export default List
---
+++ my-app/src/Pages/Home/index.js
@@ -0,0 +1,12 @@
+// import React from "react"
+import Edit from './Components/Edit'
+import List from './Components/List'
+// import Item from './Pages/Home/Components'
+
+const Home = () =>{
+    return <div>
+        <Edit/>
+
+    </div>
+} 
+export default Home
